d805904538774e7476e1e93c1c7256f5bbb8d2a4
respawn-Doubleo31 <93040948+respawn-Doubleo31@users.noreply.github.com>
2025-12-04 16:23:33 +0200

    Add files via upload

---
+++ src/scenes/GameOver.js
@@ -0,0 +1,22 @@
+export class GameOver extends Phaser.Scene {
+    constructor() {
+        super('GameOver');
+    }
+
+    create() {
+        this.cameras.main.setBackgroundColor(0xff0000);
+
+        this.add.image(512, 384, 'background').setAlpha(0.5);
+
+        this.add.text(512, 384, 'Game Over', {
+            fontFamily: 'Arial Black', fontSize: 64, color: '#ffffff',
+            stroke: '#000000', strokeThickness: 8,
+            align: 'center'
+        }).setOrigin(0.5);
+        this.add.text(256, 192, 'By C.H. Mafuleka', {
+            fontFamily: 'Arial Black', fontSize: 32, color: '#ffffff',
+            stroke: '#000000', strokeThickness: 4,
+            align: 'left'
+        }).setOrigin(0.25);
+    }
+}
